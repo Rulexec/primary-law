f54573ed77b93f5c3c0a0252d388f297ba3188b7
Alexander Ruliov <alexander.ruliov@gmail.com>
2020-06-21 23:12:37 +0300

    first two parts

---
+++ component---src-pages-404-js-60ab547cbfdfa4643402.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{w2l6:function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),w=t.n(u);e.default=function(){return w.a.createElement("div",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-60ab547cbfdfa4643402.js.map
