feb7ee1f115ddd4f7ad987d0125ef3d4132e1313
Alexander Ruliov <alexander.ruliov@gmail.com>
2020-06-22 12:14:59 +0300

    add court chapter

--- src/data/data.js
+++ src/data/data.js
@@ -6,6 +6,8 @@ export { getData };
 const preamble = readTextFileSync(resolveRootPath('data/preamble.txt'));
 const parts_1_3_1994 = readTextFileSync(resolveRootPath('data/1-3-1994.txt'));
 const parts_1_3_2004 = readTextFileSync(resolveRootPath('data/1-3-2004.txt'));
+const parts_4_1994 = readTextFileSync(resolveRootPath('data/4-1994.txt'));
+const parts_4_2004 = readTextFileSync(resolveRootPath('data/4-2004.txt'));
 const parts_5_6_1994 = readTextFileSync(resolveRootPath('data/5-6-1994.txt'));
 const parts_5_6_2004 = readTextFileSync(resolveRootPath('data/5-6-2004.txt'));
 const parts_7_8_1994 = readTextFileSync(resolveRootPath('data/7-8-1994.txt'));
@@ -16,6 +18,8 @@ function getData() {
 		preamble,
 		parts_1_3_1994,
 		parts_1_3_2004,
+		parts_4_1994,
+		parts_4_2004,
 		parts_5_6_1994,
 		parts_5_6_2004,
 		parts_7_8_1994,
--- src/data/process.js
+++ src/data/process.js
@@ -7,6 +7,8 @@ function processRawData(rawData) {
 		preamble,
 		parts_1_3_1994,
 		parts_1_3_2004,
+		parts_4_1994,
+		parts_4_2004,
 		parts_5_6_1994,
 		parts_5_6_2004,
 		parts_7_8_1994,
@@ -15,12 +17,15 @@ function processRawData(rawData) {
 
 	let ver1994_1_3 = parse(parts_1_3_1994);
 	let ver2004_1_3 = parse(parts_1_3_2004);
+	let ver1994_4 = parse(parts_4_1994);
+	let ver2004_4 = parse(parts_4_2004);
 	let ver1994_5_6 = parse(parts_5_6_1994);
 	let ver2004_5_6 = parse(parts_5_6_2004);
 	let ver1994_7_8 = parse(parts_7_8_1994);
 	let ver2004_7_8 = parse(parts_7_8_2004);
 
 	let common = merge(ver1994_1_3, ver2004_1_3);
+	common = common.concat(merge(ver1994_4, ver2004_4));
 	common = common.concat(merge(ver1994_5_6, ver2004_5_6));
 	common = common.concat(merge(ver1994_7_8, ver2004_7_8));
 
